5a9b2cc13c271de7e35fe25d02d370ed47ac78a8
tuuguu <2go.ldor@gmail.com>
2022-01-09 23:49:33 +0800

    Togloom 100% belen

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ document.querySelector(".btn-hold").addEventListener("click", function(){
         document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
 
         // Уг тоглогч хожсон эсэхийг шалгах
-        if(scores[activePlayer] >= 15){
+        if(scores[activePlayer] >= 100){
         isNewGame = false;
         // Ялагч гэсэн текстийг нэрнийнх нь оронд гаргана
         document.getElementById("name-" + activePlayer).textContent = "Winner!!!";
